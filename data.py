fbc7f475ba35540982be87ccedea110f73446bd9
Anivaries <k0m4r4c00@gmail.com>
2022-07-28 17:19:17 +0200

    update

--- data.py
+++ data.py
@@ -3,15 +3,15 @@ import requests
 import glob
 
 bracket_list = ["Herald-Crusader", "Archon", "Legend", "Ancient", "Divine-Immortal"]
-response = requests.get("https://www.dotabuff.com/heroes/meta?view=played&metric=rating_bracket", headers = {'User-agent': 'your bot 0.1'})
+response = requests.get("https://www.dotabuff.com/heroes/meta?view=played&metric=rating_bracket", headers = {'User-agent': 'your bot 0.1'}) 
 web_page = response.text
-soup = BeautifulSoup(web_page, "html.parser")
+soup = BeautifulSoup(web_page, "html.parser")                                               ## taking info from https://www.dotabuff.com ##
 
 #-------------------- FINDING HERO NAMES ------------------------------------#
-tbody_hero_names = soup.select(selector="tr td a") ##  returns table element which contains hero names, pick rates and win rates  
+tbody_hero_names = soup.select(selector="tr td a")                                          ## returns table element which contains hero names, pick rates and win rates ##
 
-tbody_hero_list = [a.get_text() for a in tbody_hero_names] ## appending names and other leftover stuff from table
-hero_names = tbody_hero_list[1::2] ## extracts names only ( filters out random stuff)
+tbody_hero_list = [a.get_text() for a in tbody_hero_names]                                  ## appending names and other leftover stuff from table ##
+hero_names = tbody_hero_list[1::2]                                                          ## extracts names only ( filters out random stuff) ###
 hero_names.sort()
 #-------------------- FINDING HERO NAMES ------------------------------------#
 
@@ -20,9 +20,9 @@ hero_img_list = glob.glob("heroes/*jpg")
 #-------------------------- HERO IMAGES ------------------------------------#
 
 #-------------------- FINDING HERO WIN RATES BY BRACKET------------------------------------#
-tbody_hero_names_win_rates = soup.select(selector="tr td") ##  shows all hero names, win rates and pick rates  
+tbody_hero_names_win_rates = soup.select(selector="tr td")                                  ## shows all hero names, win rates and pick rates ##
 
-wr_list = [a.attrs.get("data-value") for a in tbody_hero_names_win_rates if a is not None] ## same as above, this one is only for win rate ( which depends on bracket )
+wr_list = [a.attrs.get("data-value") for a in tbody_hero_names_win_rates if a is not None]  ## same as above, this one is only for win rate ( which depends on bracket ) ##
 herald_crusader_win_rate = wr_list[3::12]
 archon_win_rate = wr_list[5::12]
 legend_win_rate = wr_list[7::12]
--- ui.py
+++ ui.py
@@ -1,12 +1,15 @@
 from tkinter import *
+from turtle import bgcolor
 from quizz import QuizBrain
 from PIL import ImageTk
+import time
 
 THEME_COLOR = "#375362"
 class QuizInterface:
 
-    
+
     def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.is_pressed = bool
         self.score = 0
         self.quiz = quiz_brain
         self.window =  Tk()
@@ -17,44 +20,58 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(height=250, width=300, bg="white")
-        self.question_text = self.canvas.create_text(150,125,text="Some Question Text", fill=THEME_COLOR, font=("Arial", 10, "italic"))
+        self.question_text = self.canvas.create_text(150,125,width=330,text="Some Question Text", fill=THEME_COLOR, font=("Arial", 10, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)       
         
-        self.button_one = Button(command=self.left_hero_button, highlightthickness=0)
+        self.button_one = Button(command=self.left_hero_button, highlightthickness=1)
         self.button_one.grid(row=2, column=0)
-        self.button_two = Button(command=self.right_hero_button, highlightthickness=0)
+        self.button_two = Button(command=self.right_hero_button, highlightthickness=1)
         self.button_two.grid(row=2, column=1)
 
         self.get_next_question()
         self.window.mainloop()
 
+
+
     def get_img(self):
-        self.left_hero_img = ImageTk.PhotoImage(file=fr"{self.quiz.hero_one_img}")
+        self.left_hero_img = ImageTk.PhotoImage(file=fr"{self.quiz.hero_one_img}")                      ### pulls hero images from quizz.py ###
         self.button_one.config(self.button_one, image=self.left_hero_img)
 
         self.right_hero_img = ImageTk.PhotoImage(file=fr"{self.quiz.hero_two_img}")
         self.button_two.config(self.button_two, image=self.right_hero_img)
 
-
     def get_next_question(self):
-        q_text = self.quiz.random_picks()
-        self.canvas.itemconfig(self.question_text, text=q_text)
-        self.get_img()
+        self.button_two.config(self.button_two, bg="white", highlightthickness=1, state="normal")       ### resets buttons to normal state ( gree / red color to white####
+        self.button_one.config(self.button_one, bg="white", highlightthickness=1, state="normal")                       ### and enables them again ( state=normal )   ####
+        q_text = self.quiz.random_picks()                                                               #### next question text ###
+        self.canvas.itemconfig(self.question_text, text=q_text, )                                       #### shows text in canvas ###
+        self.get_img()                                                                                  #### updates hero images on buttons to current question ###
 
     def left_hero_button(self):
+        if self.is_pressed:
+            self.button_two.config(state=DISABLED)
         if self.quiz.winrate_hero_one > self.quiz.winrate_hero_two:
             self.score_increase()
+            self.button_one.config(self.button_one, bg="green", highlightthickness=1, state="disabled")       
         else:
             print(f"{self.quiz.hero_name_two} has higher winrate!")
-        self.get_next_question()
+            self.button_one.config(self.button_one, bg="red", highlightthickness=1, state="disabled")
+        self.window.after(1500, self.get_next_question)
 
     def right_hero_button(self):
+        if self.is_pressed:                                                                         ##### disables the other button if this one is clicked #####
+            self.button_one.config(state=DISABLED)                                                  ##### disables the other button if this one is clicked #####
         if self.quiz.winrate_hero_two > self.quiz.winrate_hero_one:
-            self.score_increase()
+            self.score_increase()                                                   #### checks the winner and increases score if correct, puts green border around the button ###
+            self.button_two.config(self.button_two, bg="green", highlightthickness=1, state="disabled")
         else:
-            print(f"{self.quiz.hero_name_one} has higher winrate!")
-        self.get_next_question()
+            print(f"{self.quiz.hero_name_one} has higher winrate!")                 ### if wrong puts red border around the button and sends message who has higher wr ###
+            self.button_two.config(self.button_two, bg="red", highlightthickness=1, state="disabled")          
+        self.window.after(1500, self.get_next_question)                             ### after 1,5 sec from clicking a button goes to next question ###
 
     def score_increase(self):
         self.score += 100
-        self.score_label.config(text=f"Score: {self.score}")
+        self.score_label.config(text=f"Score: {self.score}")                        #### updates score ### 
+
+
+    
